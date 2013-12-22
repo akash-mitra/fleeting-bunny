e8d6232a01a7346d3bc9d7658ccdd3043ec8ac14
Akash Mitra <akashmitra@gmail.com>
2013-12-23 01:00:58 +0800

    Update apache_virtual_host_template.sh

--- apache_virtual_host_template.sh
+++ apache_virtual_host_template.sh
@@ -1,9 +1,8 @@
-
 # Following lines are added by Fleeting Bunny
 <VirtualHost *:80>
     ServerAdmin __SERVER_ADMIN__
     DocumentRoot __DOCUMENT_ROOT__
     ServerName __SERVER_NAME__
     ErrorLog __ERROR_LOG__
-    CustomLog __CUSTOM_LOG__ __CUSTOM_LOG_TYPE__
+    CustomLog __CUSTOM_LOG__ combined
 </VirtualHost>
