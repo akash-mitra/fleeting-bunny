3105bae21c81be0fa3d0200fd4e7f64d564a6e55
Akash Mitra <akashmitra@gmail.com>
2014-01-01 17:52:43 +0800

    Delete apache_virtual_host_template.sh

--- apache_virtual_host_template.sh
+++
@@ -1,8 +0,0 @@
-# Following lines are added by Fleeting Bunny
-<VirtualHost *:80>
-    ServerAdmin __SERVER_ADMIN__
-    DocumentRoot __DOCUMENT_ROOT__
-    ServerName __SERVER_NAME__
-    ErrorLog __ERROR_LOG__
-    CustomLog __CUSTOM_LOG__ combined
-</VirtualHost>
